118c49978b8c106e0bc3bfafb76cb5fcf673c767
typark99 <typark99@gmail.com>
2014-08-04 22:18:36 -0500

    Update lab1

--- day1/lab1.py
+++ day1/lab1.py
@@ -9,27 +9,64 @@ def binarify(num):
    if num>=(2**i):
       digits.append('1')
       num = num - (2**i)
-  #   print num
-  #   print digits
    else:
       digits.append('0')
       
-  print ''.join(digits)
+  return ''.join(digits)
   
-binarify(130)
+#print binarify(130)
+
+## From Dave
+def binarify2(num): 
+  """convert positive integer to base 2"""
+  if num<=0: return '0'
+  digits=[]
+  while num>0:
+    digits.append(num%2)
+    num=num/2
+  digits=digits[::-1]
+  return ''.join(str(e) for e in digits)
+
+#print binarify2(130)
+
 
 def int_to_base(num, base):
   """convert positive integer to a string in any base"""
   if num<=0:  return '0' 
   digits = []
+  while num>0:
+    digits.append(num%base)
+    num=num/base
+  digits=digits[::-1]
+  return ''.join(str(e) for e in digits)
+  
+## From Dalston
+def int_to_base2(num, base):
+  """convert positive integer to a string in any base"""
+  if num<=0:  return '0' 
+  digits = []
+  for i in range(13, -1, -1):
+  	power = base**i
+  	digits.append(str(num/power))
+  	num %= power
   return ''.join(digits)
+  
+#print int_to_base(13000,2)
+#print int_to_base2(13000,2)
+#print int_to_base(400,5)
+#print int_to_base2(400,5)
+#print int_to_base(48,4)
+#print int_to_base2(48,4)
+  
 
 def base_to_int(string, base):
   """take a string-formatted number and its base and return the base-10 integer"""
   if string=="0" or base <= 0 : return 0 
   result = 0 
   return result 
-
+  
+  
+  
 def flexibase_add(str1, str2, base1, base2):
   """add two numbers of different bases and return the sum"""
   result = int_to_base(tmp, base1)
@@ -44,4 +81,3 @@ def romanify(num):
   """given an integer, return the Roman numeral version"""
   result = ""
   return result
-   
